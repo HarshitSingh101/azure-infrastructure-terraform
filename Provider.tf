88a482935a7ef6167d6850a05612794855e4e740
Harshit Singh <har7sit@gmail.com>
2026-08-20 00:14:51 +0530

    Configure remote Terraform backend and CI/CD setup

--- Provider.tf
+++ Provider.tf
@@ -1,18 +1,16 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-terraform-state"
-#     storage_account_name = "sttfstate68439131"
-#     container_name       = "tfstate"
-#     key                  = "real-project.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstateharshit2026"
+    container_name       = "tfstate"
+    key                  = "real-project.tfstate"
+  }
+}
 
 provider "azurerm" {
-
   features {}
 
   subscription_id = var.subscription_id
-
 }
 
 provider "tls" {}
